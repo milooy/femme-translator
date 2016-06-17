bb2dde66b08701e26c0fe48e4b4580080c401da6
yoolmoo <yoolmoo@smartstudy.co.kr>
2016-06-17 13:33:12 +0900

    중첩 for문을 map으로 빼고, ㅗ와 ㅏ만 바꾸던 것을 중성 두개를 쪼개도록 바꿈

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 var femme = function(str) {
-    for(var i=0; i<str.length; i++) {
-        dArr = Hangul.disassemble(str[i]);
-        if(dArr.length == 4) {
-            if(dArr[1] == 'ㅗ' && dArr[2] == 'ㅏ') {
-                var tmp = dArr['3'];
-                dArr[3] = dArr[2];
-                dArr[2] = tmp;
-            }
-            str =[str.slice(0, i), Hangul.assemble(dArr), str.slice(i+1)].join('');
+    return str.split('').map(function(char) {
+        var d = Hangul.disassemble(char);
+        if(Hangul.isVowel(d[1]) && Hangul.isVowel(d[2])) {
+            var tmp = d['3'];
+            d[3] = d[2];
+            d[2] = tmp;
         }
-    }
-    return str;
-}
+        return Hangul.assemble(d);
+    }).join('');
+};
 
 $(function() {
     $('.change').on('click', function() {
