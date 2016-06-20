89d5d32d93db3a23596ab9aa183554931d694ff5
yoolmoo <yoolmoo@smartstudy.co.kr>
2016-06-20 16:55:10 +0900

    스트링으로 들어가던 것을 넘버로 넘김

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var femme = function(str) {
     return str.split('').map(function(char) {
         var d = Hangul.disassemble(char);
         if(d[3] && Hangul.isVowel(d[1]) && Hangul.isVowel(d[2])) {
-            var tmp = d['3'];
+            var tmp = d[3];
             d[3] = d[2];
             d[2] = tmp;
         }
